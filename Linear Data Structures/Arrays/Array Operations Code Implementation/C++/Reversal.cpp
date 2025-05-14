b0474a0d9348461c9e2f6cc9847deb3ad7a1586a
Prem Kumar M <premkumarm23122005@gmail.com>
2025-05-14 20:03:42 +0530

    Create Reversal.cpp

---
+++ Linear Data Structures/Arrays/Array Operations Code Implementation/C++/Reversal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    // Reversing the array
+    int start = 0, end = size - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    cout << "Array after reversal: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+/* 
+Sample Output:
+Input:
+Enter the number of elements: 5
+Enter 5 elements: 1 2 3 4 5
+Output:
+Array after reversal: 5 4 3 2 1
+*/
