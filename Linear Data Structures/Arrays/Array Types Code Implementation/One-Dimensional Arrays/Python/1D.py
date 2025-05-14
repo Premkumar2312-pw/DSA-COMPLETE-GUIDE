952149d708470193cead5dd33821f6f10d54533f
Prem Kumar M <premkumarm23122005@gmail.com>
2025-05-14 20:12:22 +0530

    Create 1D.py

---
+++ Linear Data Structures/Arrays/Array Types Code Implementation/One-Dimensional Arrays/Python/1D.py
@@ -0,0 +1,10 @@
+# Getting user input
+arr = list(map(int, input("Enter the elements separated by space: ").split()))
+
+print("1D Array:", arr)
+
+# Sample Output:
+# Input:
+# Enter the elements separated by space: 1 2 3 4 5
+# Output:
+# 1D Array: [1, 2, 3, 4, 5]
