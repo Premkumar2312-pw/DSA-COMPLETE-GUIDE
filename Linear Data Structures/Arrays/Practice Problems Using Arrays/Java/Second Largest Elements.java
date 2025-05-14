e8f711139e50853e5c678bedff9b597d6b8a6577
Prem Kumar M <premkumarm23122005@gmail.com>
2025-05-14 22:32:53 +0530

    Create Second Largest Elements.java

---
+++ Linear Data Structures/Arrays/Practice Problems Using Arrays/Java/Second Largest Elements.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int findSecondLargest(int[] arr, int n) {
+        int largest = arr[0], secondLargest = -1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter
