ff70f170f5ba489c97aab1fe2b6c30a1e6685fb5
Prem Kumar M <premkumarm23122005@gmail.com>
2025-05-14 20:05:03 +0530

    Create Reversal.py

---
+++ Linear Data Structures/Arrays/Array Operations Code Implementation/Python/Reversal.py
@@ -0,0 +1,13 @@
+# Getting user input
+arr = list(map(int, input("Enter the elements separated by space: ").split()))
+
+# Reversing the array
+arr.reverse()
+
+print("Array after reversal:", arr)
+
+# Sample Output:
+# Input:
+# Enter the elements separated by space: 1 2 3 4 5
+# Output:
+# Array after reversal: [5, 4, 3, 2, 1]
